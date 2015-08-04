6ecd58441a46759c077ff7a65654431b4d6b5dfd
Drew Price <drewprice@users.noreply.github.com>
2015-08-04 16:32:42 -0400

    Add solution

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+  def initialize(string)
+    @string = string
+    @sorted_string = sort_chars(@string)
+  end
+
+  def match(words)
+    words.keep_if do |word|
+      @sorted_string == sort_chars(word)
+    end
+  end
+
+  private
+
+  def sort_chars(word)
+    word.chars.sort
+  end
+end
